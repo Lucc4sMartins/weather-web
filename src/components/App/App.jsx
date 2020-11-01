7c323184b56c30368d17fe49ac2a1a80f650fb2b
luccas-martins <luccas.martins@zoop.co>
2020-10-31 22:24:07 -0300

    feat: initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as Styled from './styles'
+
+const App = () => (
+  <Styled.P>
+    Initial App
+  </Styled.P>
+)
+
+export { App }
---
+++ src/components/App/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const P = styled.p`
+  text-transform: uppercase;
+  font-weight: bold;
+`
+
+export {
+  P
+}
