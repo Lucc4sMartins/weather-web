4762e1822bf52bfdfb2573a76ced291475821943
Luccas Martins <luccasmartins.nave@gmail.com>
2020-11-01 19:53:33 -0300

    feat: add google maps search box

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { GoogleMap, LoadScript, Marker, StandaloneSearchBox } from '@react-google-maps/api'
 import { setLocation } from '@/ducks/Location'
 import useWindowSize from '@/hooks/useWindowsSize'
-
-const center = {
-  lat: -34.397,
-  lng: 150.644
-}
+import * as Styled from './styles'
 
 const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
+const libraries = ['places']
+
 const Map = () => {
+  const [searchBoxRef, setSearchBoxRef] = useState(null)
+  const [center, setCenter] = useState({
+    lat: -22.9068467,
+    lng: -43.1728965
+  })
   const dispatch = useDispatch()
   const { width } = useWindowSize()
   const { lat, lng } = useSelector(({ location: { lat, lng } }) => ({
@@ -23,18 +26,30 @@ const Map = () => {
     dispatch(setLocation(lat(), lng()))
   }
 
+  const handlePlacesChange = () => {
+    const { lat, lng } = searchBoxRef.getPlaces()[0].geometry.location;
+    setCenter({ lat: lat(), lng: lng() })
+  }
+
   const containerStyle = {
     width: width < 1024 ? '100vw' : '70vw',
     height: width < 1024 ? '55vh' : 'calc(100vh - 50px)'
   }
 
   return (
-    <LoadScript googleMapsApiKey={googleMapsApiKey}>
+    <LoadScript googleMapsApiKey={googleMapsApiKey} libraries={libraries}>
       <GoogleMap
         onClick={handleMapClick}
         mapContainerStyle={containerStyle}
         center={center}
         zoom={10}>
+          <StandaloneSearchBox
+            onLoad={ref => { setSearchBoxRef(ref) }}
+            onPlacesChanged={handlePlacesChange}>
+              <Styled.Input 
+                type='text'
+                placeholder='Search on Google Maps' />
+          </StandaloneSearchBox>
         {lat && lng && (
           <Marker position={{ lat, lng }} />
         )}
---
+++ src/components/Map/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+  width: 240px;
+  height: 32px;
+  padding: 0 12px;
+  border-radius: 3px;
+  font-size: 14px;
+  outline: none;
+  border: none;
+  text-overflow: ellipses;
+  position: absolute;
+  left: 10px;
+  top: 70px;
+`
+
+export { Input }
